09d06d0c868ed0e6f0853e3fdb498a47e86af2a2
GoetteVictoria <victoria.goette01@htl-salzburg.ac.at>
2019-11-02 19:34:46 +0100

    created and renamed object, added script

--- Assets/MyGame/Scripts/HelloWorldHome.cs
+++ Assets/MyGame/Scripts/HelloWorldHome.cs
@@ -1,13 +1,8 @@
 ﻿using UnityEngine;
 
-public class HelloWorldHome : MonoBehaviour
-{
-    
+public class HelloWorldHome : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log(“Hello World at home“);
+    void Start () {
+        Debug.Log ("Hello World at home");
     }
-
-    
 }
